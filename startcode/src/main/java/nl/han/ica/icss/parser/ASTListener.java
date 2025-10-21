af28e5509cce3f4796b55854e7c699330846302a
Taha Dinc <124564206+TahaDinc@users.noreply.github.com>
2025-10-21 20:37:07 +0200

    enter, exit van stylesheet en rule methodes gemaakt

--- startcode/src/main/java/nl/han/ica/icss/parser/ASTListener.java
+++ startcode/src/main/java/nl/han/ica/icss/parser/ASTListener.java
@@ -3,6 +3,7 @@ package nl.han.ica.icss.parser;
 import java.util.Stack;
 
 
+import nl.han.ica.datastructures.HANStack;
 import nl.han.ica.datastructures.IHANStack;
 import nl.han.ica.icss.ast.*;
 import nl.han.ica.icss.ast.literals.*;
@@ -17,7 +18,7 @@ import nl.han.ica.icss.ast.selectors.TagSelector;
  * This class extracts the ICSS Abstract Syntax Tree from the Antlr Parse tree.
  */
 public class ASTListener extends ICSSBaseListener {
-	
+
 	//Accumulator attributes:
 	private AST ast;
 
@@ -26,10 +27,27 @@ public class ASTListener extends ICSSBaseListener {
 
 	public ASTListener() {
 		ast = new AST();
-		//currentContainer = new HANStack<>();
+		currentContainer = new HANStack<>();
 	}
     public AST getAST() {
         return ast;
     }
-    
+
+	@Override public void enterStylesheet(ICSSParser.StylesheetContext ctx) {
+		Stylesheet stylesheet = new Stylesheet();
+		currentContainer.push(stylesheet);
+	}
+	@
+Override public void exitStylesheet(ICSSParser.StylesheetContext ctx) {
+		Stylesheet stylesheet = (Stylesheet) currentContainer.pop();
+		ast.root = stylesheet;
+	}
+	@Override public void enterStylerule(ICSSParser.StyleruleContext ctx) {
+		Stylerule stylerule = new Stylerule();
+		currentContainer.push(stylerule);
+	}
+	@Override public void exitStylerule(ICSSParser.StyleruleContext ctx) {
+		Stylerule stylerule = (Stylerule) currentContainer.pop();
+		currentContainer.peek().addChild(stylerule);
+	}
 }
